56c67dd2c097cb35c325f1d4b0ff139e9fc1802d
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-01-16 23:16:12 +0500

    init: start sleppnest project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      {/* <Hero/> */}
+      {/* <Services/> */}
+      {/* <Qualiyt/> */}
+      {/* <Product /> */}
+      {/* <Qualiyt/> */}
+      {/* <Product /> */}
+      {/* <Qualiyt/> */}
+      {/* <Product /> */}
+      {/* <News /> */}
+    </>
+  );
+}
