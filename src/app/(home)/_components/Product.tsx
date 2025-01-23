78932fa1b06448f22afab7cfd568b0e52b4c8bbf
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-01-23 23:25:25 +0500

    feat: add products data

--- src/app/(home)/_components/Product.tsx
+++ src/app/(home)/_components/Product.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { products } from "@/db/db";
 import Image from "next/image";
 
 import Link from "next/link";
@@ -8,27 +9,31 @@ interface ProductProps {
   season: string;
 }
 
-const Product: React.FC<ProductProps> = ({season}) => (
+const Product: React.FC<ProductProps> = ({season}) => {
+  const cards = products.filter(item => {
+    return item.season === season;
+  })
+  return(
   <section className="product">
     <div className="product__start">
       <h2 className="product__title">{season} Collection</h2>
       <p className="product__description">Ecologically Clean Sleep Products</p>
       <ul className="product__list">
-        {[1,2,3,4,5,6,7,8,9,10].map((_, i) => (
-          <li className="product__list-item" key={i}>
+        {cards.map((item) => (
+          <li className="product__list-item" key={item.id}>
             <Link href="/product/11">
-              <Image className="product__list-img" src="/productImg.jpg" alt="product__image" width="250" height="200"/>
+              <Image className="product__list-img" src={item.img} alt="product__image" width="250" height="200"/>
             </Link>
             <button className="product__heart">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path></svg>
             </button>
-            <p className="product__list-text">Safari</p>
+            <p className="product__list-text">{item.title}</p>
           </li>
         ))}
       </ul>
     </div>
   </section>
-);
+)};
 
 
 export default Product;
--- src/db/db.ts
+++ src/db/db.ts
@@ -36,14 +36,187 @@ export const qualities = [
   }
 ]
 
+
 export const products = [
   {
-    season: "Winter",
+    id: 1,
+    img: '/beds/productImg1.jpg',
+    title: 'Squares',
+    season: 'Winter',
   },
   {
-    season: "Autumn"
+    id: 2,
+    img: '/beds/productImg2.jpg',
+    title: 'Ikat',
+    season: 'Winter',
   },
   {
-    season: "Summer"
+    id: 3,
+    img: '/beds/productImg3.jpg',
+    title: 'Circles',
+    season: 'Winter'
+  },
+  {
+    id: 4,
+    img: '/beds/productImg4.jpg',
+    title: 'Geometry Blue',
+    season: 'Winter'
+  },
+  {
+    id: 5,
+    img: '/beds/productImg5.jpg',
+    title: 'Tropical Leaves',
+    season: 'Winter'
+  },
+
+  {
+    id: 6,
+    img: '/beds/productImg6.jpg',
+    title: 'Bambi',
+    season: 'Winter'
+  },
+  {
+    id: 7,
+    img: '/beds/productImg7.jpg',
+    title: 'Byzantium',
+    season: 'Winter'
+  },
+  {
+    id: 8,
+    img: '/beds/productImg8.jpg',
+    title: 'Evening Garden',
+    season: 'Winter'
+  },
+  {
+    id: 9,
+    img: '/beds/productImg9.jpg',
+    title: 'Pat Marquis',
+    season: 'Winter'
+  },
+  {
+    id: 10,
+    img: '/beds/productImg10.jpg',
+    title: 'Lavender Roses',
+    season: 'Winter'
+  },
+  {
+    id: 11,
+    img: '/beds/productImg11.jpg',
+    title: 'Safari',
+    season: 'Autumn'
+  },
+  {
+    id: 12,
+    img: '/beds/productImg12.jpg',
+    title: 'Multicolor Amethyst',
+    season: 'Autumn'
+  },
+  {
+    id: 13,
+    img: '/beds/productImg13.jpg',
+    title: 'Shivali decoration',
+    season: 'Autumn'
+  },
+  {
+    id: 14,
+    img: '/beds/productImg14.jpg',
+    title: 'Emerald (AB)',
+    season: 'Autumn'
+  },
+  {
+    id: 15,
+    img: '/beds/productImg15.jpg',
+    title: 'Malachite Box',
+    season: 'Autumn'
+  },
+  {
+    id: 16,
+    img: '/beds/productImg16.jpg',
+    title: 'Scents of Spring',
+    season: 'Autumn'
+  },
+  {
+    id: 17,
+    img: '/beds/productImg17.jpg',
+    title: 'Falling autumn leaves (AB)',
+    season: 'Autumn'
+  },
+  {
+    id: 18,
+    img: '/beds/productImg18.jpg',
+    title: 'Blue Breeze',
+    season: 'Autumn'
+  },
+  {
+    id: 19,
+    img: '/beds/productImg19.jpg',
+    title: 'Blue Water',
+    season: 'Autumn'
+  },
+  {
+    id: 20,
+    img: '/beds/productImg20.jpg',
+    title: 'Plaid',
+    season: 'Autumn'
+  },
+  {
+    id: 21,
+    img: '/beds/productImg21.jpg',
+    title: 'Vintage Style',
+    season: 'Summer'
+  },
+  {
+    id: 22,
+    img: '/beds/productImg22.jpg',
+    title: 'rocking dandelion',
+    season: 'Summer'
+  },
+  {
+    id: 23,
+    img: '/beds/productImg23.jpg',
+    title: 'French Riviera',
+    season: 'Summer'
+  },
+  {
+    id: 24,
+    img: '/beds/productImg24.jpg',
+    title: 'Dolly the Sheep',
+    season: 'Summer'
+  },
+  {
+    id: 25,
+    img: '/beds/productImg25.jpg',
+    title: 'Alpine plants',
+    season: 'Summer'
+  },
+  {
+    id: 26,
+    img: '/beds/productImg26.jpg',
+    title: 'Inspirational',
+    season: 'Summer'
+  },
+  {
+    id: 27,
+    img: '/beds/productImg27.jpg',
+    title: 'Green Lotus',
+    season: 'Summer'
+  },
+  {
+    id: 28,
+    img: '/beds/productImg28.jpg',
+    title: 'Mountain Corn',
+    season: 'Summer'
+  },
+  {
+    id: 29,
+    img: '/beds/productImg29.jpg',
+    title: 'Sakura Branch',
+    season: 'Summer'
+  },
+  {
+    id: 30,
+    img: '/beds/productImg30.jpg',
+    title: 'Olive Branch',
+    season: 'Summer'
   }
 ]
