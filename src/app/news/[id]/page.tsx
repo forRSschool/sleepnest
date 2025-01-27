c90acaf4ffbb52b6a982a5a8c1f3c0f5924b0fcf
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-01-27 13:38:30 +0500

    feat: add news page

---
+++ src/app/news/[id]/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+const SingleNews = () => {
+  return (
+    <div>
+      <Image className="news__list-img" src="/newsImg1.png" alt="product__image" width="100" height="100"/>
+    </div>
+  )
+}
+
+export default SingleNews
